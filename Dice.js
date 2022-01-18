6778c325a3e4357187b8920fa02d9c05062686d0
NantokaGames <97860795+NantokaGames@users.noreply.github.com>
2022-01-18 21:20:11 +0900

    Update Dice.js

--- Dice.js
+++ Dice.js
@@ -3,20 +3,18 @@ var WebDice = function() {
     r = Math.floor(Math.random() * num);
     a = 1
     document.write('<div class=("again")><input type="button"value="もう一度サイコロを振る" onclick="Again()"></div>')
-    document.write('<div class=("again")><input type="button"value="ホームへ" onclick="home()"></div>')
-    document.write('<div class=("again")><input type="button"value="リセット" onclick="Delete()"></div>')
     document.write('<img src="images/' + r + '.png" class"dice" />')
+}
+var home = function() {
+    document.write('<h1>webダイス</h1>')
+    document.write('<p>web上で簡単に実行できるダイスです</p>')
+    document.write('<div class="dice"><input type="button" value="サイコロを振る" onclick="WebDice()"></div>')
 }
 var Again = function() {
     num = 6;
     r = Math.floor(Math.random() * num);
     a = 1
     document.write('<ul id="dice"><img src="images/' + r + '.png" class"dice" /><ui>')
-}
-var home = function() {
-    document.write('<h1>webダイス</h1>')
-    document.write('<p>web上で簡単に実行できるダイスです</p>')
-    document.write('<div class="dice"><input type="button" value="サイコロを振る" onclick="WebDice()"></div>')
 }
 var Delete = function() {
     var list_element = document.getElementById("dice");
